dd7dfe1dabdfbbbcb3c0914e2befc783be347d9a
Tonsofnamesz <Jonathanryan2015@gmail.com>
2024-03-23 11:32:45 +0700

    updated the homepage again

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-xxxxxx" crossorigin="anonymous" />
     <title>Koperasi Website</title>
 </head>
 <body>
