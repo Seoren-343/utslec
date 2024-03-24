48dd39bea7879cea29075b79bd329345e259a18c
D4rrylPrt <112367562+Seoren-343@users.noreply.github.com>
2024-03-24 12:05:58 +0700

    last change

--- db_config.php
+++ db_config.php
@@ -5,3 +5,8 @@ $password = "";
 $database = "uts_webprog_lec";
 
 $conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
